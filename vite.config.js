0b4d32a83e7db8fc65b2a341f6e4ebca37fa15d5
吴家通 <15263314285@163.com>
2022-08-30 15:11:49 +0800

    chore: env

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,63 @@
-import { defineConfig } from 'vite'
-
-const path = require('path')
-const { createVuePlugin } = require('vite-plugin-vue2')
+import { defineConfig, loadEnv } from 'vite'
+import path from 'path'
+import { createVuePlugin } from 'vite-plugin-vue2'
 import { svgBuilder } from './src/plugins/svgBuilder'
 import WindiCSS from 'vite-plugin-windicss'
-
+// console.log(loadEnv(import.meta.env))
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    createVuePlugin({
-      vueTemplateOptions: {
-        compilerOptions: {
-          // 去掉 回车 引起的 margin
-          whitespace: 'condense',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+  return {
+    plugins: [
+      createVuePlugin({
+        vueTemplateOptions: {
+          compilerOptions: {
+            // 去掉 回车 引起的 margin
+            whitespace: 'condense',
+          },
         },
-      },
-    }),
-    svgBuilder('./src/icons/svg/'),
-    WindiCSS(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  optimizeDeps: {
-    exclude: ['@/assets/fonts'],
-  },
-  base: '/',
-  server: {
-    port: 5277,
-    host: '0.0.0.0',
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'static',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'element-ui': ['element-ui'],
-          // lodash: ["lodash"],
-        },
+      }),
+      svgBuilder('./src/icons/svg/'),
+      WindiCSS(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    optimizeDeps: {
+      exclude: ['@/assets/fonts'],
+    },
+    base: '/',
+
+    server: {
+      port: 5277,
+      host: '0.0.0.0',
+      open: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_BASE_API,
+          secure: false, // 如果是https接口，需要配置这个参数
+          changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        },
+        '/auth': {
+          target: env.VITE_APP_BASE_API,
+          secure: false, // 如果是https接口，需要配置这个参数
+          changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        },
+      },
+    },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'static',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'element-ui': ['element-ui'],
+            // lodash: ["lodash"],
+          },
+        },
+      },
+    },
+  }
 })
